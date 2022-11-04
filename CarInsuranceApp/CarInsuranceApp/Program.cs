9c3d0062c66cdd8b0d9af4d98194c42e0cb1685f
Michael <michael.jlotter@gmail.com>
2022-11-04 11:26:10 -0400

    Insurance App

---
+++ CarInsuranceApp/CarInsuranceApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace CarInsuranceApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Incredible Insurance");
+            Console.WriteLine("How old are you?");
+            int AgeIs = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Answer true or false");
+            bool DuiCharge = Convert.ToBoolean(Console.ReadLine());
+            if (DuiCharge == true) //I do not know how to break out of the app and go straight to the not qualified part if this was true.
+            {
+                Console.WriteLine("You are disqualified from our insurance program. Good Luck Though");
+            }
+            else if (DuiCharge == false)
+            {
+                Console.WriteLine("One more question");
+            }
+            Console.WriteLine("How many speeding tickets do you have?");
+            int SpeedTicket = Convert.ToInt32(Console.ReadLine());
+           
+
+            if (AgeIs > 15 && DuiCharge == false && SpeedTicket <= 3)
+            {
+                Console.WriteLine("You are Qualified for our insurance Package");
+            }
+            else
+            {
+                Console.WriteLine("We cannot offer you insurance at this time");
+            }
+            Console.ReadLine();
+        }
+    }
+}
