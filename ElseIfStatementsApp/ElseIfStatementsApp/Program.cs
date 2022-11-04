25594ed5265b9d5823b5f2835f9d85f3172ebec4
Michael <michael.jlotter@gmail.com>
2022-11-04 15:59:51 -0400

    Package quote App

---
+++ ElseIfStatementsApp/ElseIfStatementsApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace ElseIfStatementsApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string FirstName = "Mike";
+            int AgeIs = 120;
+
+            if (FirstName == "Mark" && AgeIs > 50)
+            {
+                Console.WriteLine("You are not who we are looking for");
+            }
+            else if (FirstName == "Mike" && AgeIs < 50)
+            {
+                Console.WriteLine("You are not old enough to be who we are loking for");
+            }
+            else if (FirstName == "Mike" && AgeIs > 50)
+            {
+                Console.WriteLine("Just the person we were looking for");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PackageQuote/PackageQuote/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace PackageQuote
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int Maxweight = 50;
+            int MaxDimensons = 500; //I increased the dimensions because it kept going over 50 to get any kind of larger shipping cost
+            Console.Write("Welcome to Package express. Press enter and follow the instructions below.");
+            string Welcome = Console.ReadLine();
+            Console.WriteLine("Please enter the weight of he package"); //if you insert a package weight over 50 the app skips the rest of the code and goes straight to Thank youexits
+            int PackWeight = Convert.ToInt32(Console.ReadLine());
+            if (PackWeight > Maxweight)
+            {
+                Console.WriteLine("Package is too heavy to ship with Package Express.");
+                Console.ReadLine();
+            }
+            else if (PackWeight <= Maxweight)
+            {
+                Console.WriteLine("Please enter the width of the package");
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter Package Height");
+                int height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package length");
+                int length = Convert.ToInt32(Console.ReadLine());
+                int ActDimensions = width * height * length;
+                int quote = (ActDimensions * Maxweight) / 100;
+               if (ActDimensions > MaxDimensons) //I could probably use a teneray in here because it appears I ask the same question twice?
+                {
+                    Console.WriteLine("Package too big to ship with Package Express");
+                    Console.ReadLine();
+                }
+                else if (ActDimensions <= MaxDimensons)
+                {
+                    Console.WriteLine("The estimated cost to ship your package is $" + quote);
+                }
+
+            }
+            Console.WriteLine("Thank You!");
+            Console.ReadLine();
+        }
+    }
+}
