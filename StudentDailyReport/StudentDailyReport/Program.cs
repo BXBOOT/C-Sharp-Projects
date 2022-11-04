a5a8ac0c03b3deb0c9211834e90297900313a1f4
Michael <michael.jlotter@gmail.com>
2022-11-04 10:41:45 -0400

    corrected Line 18

--- StudentDailyReport/StudentDailyReport/Program.cs
+++ StudentDailyReport/StudentDailyReport/Program.cs
@@ -15,7 +15,11 @@ namespace StudentDailyReport
             Console.WriteLine("What page number are you on?");
             int PageNum = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Do you need help with anything speific? PLEASE ANSWER \"TRUE OR FALSE\"");
-            Console.ReadLine();
+            bool Answer1 = Convert.ToBoolean(Console.ReadLine());
+            if (Answer1 == true)
+            {
+                Console.WriteLine("A teacher will be in touch shortly");
+            }
             Console.WriteLine("Were there any positive experiences you would like to share? Please give specifics");
             string FeedBack = Console.ReadLine();
             Console.WriteLine("Is there any other feedback you would like to provide? Pease be specific");
