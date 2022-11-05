976098dc94ca936e5112acfca53af6cbadc3b743
Michael <michael.jlotter@gmail.com>
2022-11-04 22:25:46 -0400

    While Do While

---
+++ WhileAndDoWhile/WhileAndDoWhile/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace WhileAndDoWhile
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //int MyNum = 0;
+            //while (MyNum < 10)
+            //{
+            //    Console.WriteLine(MyNum);
+            //    MyNum++;
+            //}
+            //Console.ReadLine();
+
+            int MyNum = 0;
+                do
+            {
+                Console.WriteLine(MyNum);
+                MyNum++;
+            }
+            while (MyNum < 10);
+            Console.ReadLine();
+        }
+    }
+}
