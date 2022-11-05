ecb08ec8e9b02ac025968dc9c4fa74e6d63c08be
Michael <michael.jlotter@gmail.com>
2022-11-05 12:37:34 -0400

    String Assignment App

---
+++ StringAssignApp/StringAssignApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+
+namespace StringAssignApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string MyName = "Mike";
+            string FavMovie = "One Flew Over the Cuckoos Nest";
+            string FavSnack = "is anything chocolate";
+
+            Console.WriteLine("Hello, my name is " + MyName + ". My fvourite movie is " + FavMovie + ". My favourite quote frm the movie is \n \"Here's Johnny\".");
+           
+
+            MyName = MyName.ToUpper();
+            Console.WriteLine(MyName);
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My Name is Mike");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
