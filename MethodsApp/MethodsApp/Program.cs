5eaa124b1dfdede3ab2e64136b88f095fba71b3e
Michael <michael.jlotter@gmail.com>
2022-11-15 19:16:16 -0500

    Method App

---
+++ MethodsApp/MethodsApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int value = Convert.ToInt32(Console.ReadLine());
+
+            Threes obj = new Threes();
+            obj.Adder(x);
+            
+            Console.WriteLine("The answer is " + obj + value);
+            Console.ReadLine();
+            
+        }
+        
+    }
+}
---
+++ MethodsApp/MethodsApp/Threes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsApp
+{
+   public class Threes
+    {
+        
+        public static int Adder(int x)
+        {
+            return x = 10;
+        }
+     }
+}
