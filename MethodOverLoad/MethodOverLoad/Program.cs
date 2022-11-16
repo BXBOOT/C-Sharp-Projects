24cbdbd0bb89df0144fb9e3d19f77ef835211def
Michael <michael.jlotter@gmail.com>
2022-11-16 11:38:41 -0500

    Over Load Methods

---
+++ MethodOverLoad/MethodOverLoad/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverLoad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Step1 step = new Step1();
+            Console.WriteLine("The answer is " + step.Add(5, 3));
+            Console.WriteLine("The answer is " + step.Add(5, 3));
+            Console.WriteLine("The answer is, " + step.Add("", ""));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodOverLoad/MethodOverLoad/Step1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverLoad
+{
+    public class Step1
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double Add(double a, double b)
+        {
+            return a / b;
+        }
+
+        public string Add(string a, string b)
+        {
+            return "Is this " + " going to work?";
+        }
+    }
+}
