26122b78a11a6c102e545bf947e343a7eec174f6
Michael <michael.jlotter@gmail.com>
2022-11-17 11:16:45 -0500

    user input value

--- MethodOverLoad/MethodOverLoad/Program.cs
+++ MethodOverLoad/MethodOverLoad/Program.cs
@@ -12,13 +12,16 @@ namespace MethodOverLoad
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("enter a number");
+            int value = Convert.ToInt32(Console.ReadLine());
+
             Step1 step = new Step1();// create the object named step
 
-            Console.WriteLine("The answer is, " + step.Add(5, 3));// return an int result
+            Console.WriteLine("The answer is, " + step.Add(5, value));// return an int result
 
             Console.WriteLine("The answer is, " + step.Add("b", "22"));// conversion done in method with try/catch theat prints -1 and message
 
-            Console.WriteLine("The answer when converting a decimal to int is, " + step.Add(27.5m, 7.5m));//answer will be whole number because of int conversion
+            Console.WriteLine("The answer when converting a decimal to int is, " + step.Add(27.5m, value));//answer will be whole number because of int conversion
 
             Console.ReadLine();
         }
