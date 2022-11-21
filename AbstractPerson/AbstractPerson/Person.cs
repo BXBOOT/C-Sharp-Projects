730ebfbda8371865136326b1a6a053aba58e09bf
Michael <michael.jlotter@gmail.com>
2022-11-21 12:49:29 -0500

    Abstract person

---
+++ AbstractPerson/AbstractPerson/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractPerson
+{
+    abstract class Person
+    {
+        public string FirstName { get; set; }//parameter set for abstract class
+        public string LastName { get; set; }
+
+        public abstract void SayName();//abstract method does not have a body
+    }     
+
+        class Employee : Person//new class employee inherits from abstract person class
+        {
+            public override void SayName()// the override method from the abstract class so it can be called by the employee class
+            {
+                Console.WriteLine("Name : " + FirstName + " " + LastName);//what the method should do when called in main program
+            }
+        }
+    
+}
---
+++ AbstractPerson/AbstractPerson/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractPerson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Employee employee = new Employee();//instnatiate the class employee which inherts from the abstract person class
+            employee.FirstName = "Sample";//use get ad set for parameters
+            employee.LastName = "Student";
+            employee.SayName();//call the method
+            Console.ReadLine();//print to console
+        }
+    }
+}
