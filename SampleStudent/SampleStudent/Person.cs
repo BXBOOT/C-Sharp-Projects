f478743dc5cb9b2aeca074f28b9da1fb63b96e8e
Michael <michael.jlotter@gmail.com>
2022-11-21 10:49:42 -0500

    inheritance

---
+++ SampleStudent/SampleStudent/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleStudent
+{
+    public class Person 
+    {
+        public string FirstName { get; set; } //setproperty of the person class
+        public string LastName { get; set; }//set property of the person class
+
+        public void SayName()//give the person class a method with no parameters
+        {
+            {
+                Console.WriteLine("Name: " + FirstName + " " + LastName);//required in method to print to console
+            }
+        }
+    }
+
+
+    public class  Employee : Person //inherit from the peson class to the emplyee class. employee can now be used for the program
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ SampleStudent/SampleStudent/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleStudent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();//instantiate the employee class. i inherits all the person class attributes
+            employee.FirstName = "Sample"; //can enter whatevr name to here
+            employee.LastName = "Student"; //can enter whatever name to here
+            employee.SayName();//calls the method to prit to console
+
+            Console.ReadLine();
+
+        }
+    }
+}
