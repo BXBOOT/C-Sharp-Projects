2d30aff22540fdc727d06aef3849cc1b51f27ee6
Michael <michael.jlotter@gmail.com>
2022-11-23 20:15:56 -0500

    fixed bool return

--- OperatorsApp/OperatorsApp/Employee.cs
+++ OperatorsApp/OperatorsApp/Employee.cs
@@ -12,14 +12,22 @@ namespace OperatorsApp
         public string LastName { get; set; }//property for LastName
         public int Id { get; set; }//property for id
 
-        public static Employee operator== (Employee Id, Employee LastName)//overide operator to check if equal
+        public static bool operator== (Employee emp1 , Employee emp2 )//overide operator to check if equal
         {
-            return Id;
+            if (emp1.Id.Equals(emp2.Id))//compares the emp1 and emp 2 to be equal id or not in an if statement
+            {
+                return true;//should return true if they are equal
+            }
+            return false;//must return opposite if the return value of true is not met
         }
 
-        public static Employee operator!= (Employee Id, Employee LastName)//verride operator to check if not equal. done in pairs.
+        public static bool operator !=(Employee emp1, Employee emp2)//overide operator to check if equal
         {
-            return Id;
+            if (!emp1.Id.Equals(emp2.Id))//! has to preceed the if statement or it throws an error
+            {
+                return true;//should eturn true if they are not equal
+            }
+            return false;
         }
     }
 }
--- OperatorsApp/OperatorsApp/Program.cs
+++ OperatorsApp/OperatorsApp/Program.cs
@@ -10,6 +10,27 @@ namespace OperatorsApp
     {
         static void Main(string[] args)
         {
+            Employee emp1 = new Employee();//instantiate 1 employye
+            emp1.FirstName = "Billy";//params as set out in class
+            emp1.LastName = "Bob";//params as set out in class
+            emp1.Id = 456;
+
+            Employee emp2 = new Employee();//second employee instantiate
+            emp2.FirstName = "Billy";//params as set out in class
+            emp2.LastName = "Rob";//params as set out in class
+            emp2.Id = 456;
+
+
+            if (emp1 == emp2)//comparing the entirety of employee 1 to employee 2
+            {
+                Console.WriteLine("These two emplyees are twins");//make changes to see different output
+            }
+            else
+            {
+                Console.WriteLine("These 2 might look alike but they are not the same");
+            }
+
+            Console.ReadLine();
         }
     }
 }
